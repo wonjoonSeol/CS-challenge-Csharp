3b73514594cd282ce7ed9d3ae00d2a33d996698e
Wonjoon Seol <wonjoon.seol@gmail.com>
2018-10-31 06:49:38 +0000

    remove comments

--- Answers/Question5.cs
+++ Answers/Question5.cs
@@ -7,20 +7,13 @@
             int result = minAllocations(numOfShares, totalValueOfShares);
             return result == int.MaxValue ? 0 : result;
         }
-
-
         public static int minAllocations(int[] allowedAllocations, int total) {
             int[] memo = new int[total + 1];
-            // Base case (If given value V is 0)
             memo[0] = 0;
-            // Initialize all table values as Infinite
             for (int i = 1; i <= total; i++) {
                 memo[i] = int.MaxValue;
             }
-            // Compute minimum coins required for all
-            // values from 1 to total
             for (int i = 1; i <= total; i++) {
-                // Go through all coins smaller than i
                 for (int j = 0; j < allowedAllocations.Length; j++) {
                     if (allowedAllocations[j] <= i) {
                         int temp = memo[i - allowedAllocations[j]];
